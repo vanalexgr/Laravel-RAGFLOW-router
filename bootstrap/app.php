8da0ff36f351cf705560074e6078efca4a4efc2f
vanalexgr <alexiouv@gmail.com>
2026-01-21 23:30:19 +0200

    Configure trusted proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->append(\App\Http\Middleware\HttpLogging::class);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
